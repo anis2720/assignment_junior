c18540720c6fb59a268d809f4be032ddd6aa9d69
anis2720 <138358919+anis2720@users.noreply.github.com>
2024-08-21 01:07:29 +0800

    Update Calculation.php

--- Calculation.php
+++ Calculation.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Calculate</title>
 
 <style>
-	table {
-		font-family: arial, sans-serif;
-		width: 100%;
-	}
+    table {
+        font-family: arial, sans-serif;
+        width: 100%;
+    }
 
-	td, th {
-		border-bottom: 1px solid ;
-		border-bottom-color: grey;
- 		text-align: left;
-  		padding: 5px;
-	}
+    td, th {
+        border-bottom: 1px solid ;
+        border-bottom-color: grey;
+        text-align: left;
+        padding: 5px;
+    }
 
 </style>
 
@@ -53,49 +53,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     $current = isset($_POST['current']) && is_numeric($_POST['current']) ? floatval($_POST['current']) : 0;
     $crate = isset($_POST['crate']) && is_numeric($_POST['crate']) ? floatval($_POST['crate']) : 0;
 
-
-    if ($voltage > 0 && $current > 0 && $crate > 0) {
+    
+    function calculatepowerandcost($voltage, $current, $crate)
+    {
+        if ($voltage > 0 && $current > 0 && $crate > 0) {
 
         $power = $voltage * $current;
 
         $powerKW = $power /1000;
-    	$rate = $crate / 100;
+        $rate = $crate / 100;
 
-    	echo "<p>POWER: $powerKW  kw</p>";
-    	echo "<p>RATE: $rate RM</p><br>";
+        echo "<p>POWER: $powerKW  kw</p>";
+        echo "<p>RATE: $rate RM</p><br>";
 
-    	echo "<table>";
+        echo "<table>";
         echo "<tr><th>#</th><th>Hour</th><th>Energy(kWh)</th><th>TOTAL(RM)</th></tr>";
 
 
-    	for ($hours=1; $hours <=24 ; $hours++) 
-    	{     	
+        for ($hours=1; $hours <=24 ; $hours++) 
+        {       
 
-    	$energy = $power * $hours * 1000;
-    	$total = $energy * ($crate / 100);
+        $energy = $power * $hours * 1000;
+        $total = $energy * ($crate / 100);
 
-    	$totalC = $total / 1000000;
-    	$energyC = $energy / 1000000; 
+        $totalC = $total / 1000000;
+        $energyC = $energy / 1000000; 
 
-    	echo "<tr>";
-    	echo "<td><b>$hours</td>";
-    	echo "<td>$hours</td>";
-    	echo "<td>$energyC</td>";
-    	echo "<td>" . number_format($totalC, 2) . "</td>";
-    	echo "</tr>";
+        echo "<tr>";
+        echo "<td><b>$hours</td>";
+        echo "<td>$hours</td>";
+        echo "<td>$energyC</td>";
+        echo "<td>" . number_format($totalC, 2) . "</td>";
+        echo "</tr>";
 
-    	}
+        }
 
-    	echo "</table>";
-    }       
+        echo "</table>";
 
-    else {
+        }       
 
-        echo "<p>Please enter values for voltage, current, and rate.</p>";
-    }    
+        else {
+
+            echo "<p>Please enter values for voltage, current, and rate.</p>";
+        }    
+    }
+
+    calculatepowerandcost($voltage, $current, $crate);    
 
 }
+
 ?>
+
 </div>    
 </body>
 </html>
